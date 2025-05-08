b16c417177d61ebd285005b298d52b7527b8ab2a
Belawal khan <Belawal.khan@gmail.com>
2025-05-08 15:32:46 +0000

    added contexts and bag template

--- bag/urls.py
+++ bag/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'bag'
-
 urlpatterns = [
-  path('', views.view_bag, name='view_bag'),\
+    path('', views.view_bag, name='view_bag'),
 ]
