98bdeaa41ee9a9a81edf637621ffdf9f30de2334
Belawal khan <Belawal.khan@gmail.com>
2025-05-15 10:35:23 +0000

    debug to false

--- ovella_online/settings.py
+++ ovella_online/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-belawal-ovellap5-g05qeqslouz.ws-us118.gitpod.io',
